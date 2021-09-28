b45646edb1f49522d7eb2255ac4d399a3bef9e7e
Renan Azevedo <renancardan@hotmail.com>
2021-09-28 13:31:51 -0300

    modulo inicial

---
+++ src/routes/api.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+
+const router = Router();
+
+router.get('/ping', (req, res)=>{
+    res.json({pong:true});
+});
+
+router.get('/random', (req, res)=>{
+    let nRand: number = Math.floor( Math.random() * 10);
+    res.json({number:nRand});
+});
+
+router.get('/nome/:nome', (req, res)=>{
+    let Nome: string = req.params.nome;
+    res.json({Nome});
+});
+
+export default router;
